8b4a3b171762c3986c2286f74ca45e69f53e6dc9
thai-phan <thaiphan2712@gmai.com>
2023-06-11 22:32:14 +0900

    update UI

--- app/src/main/java/com/sewon/healthmonitor/MainActivity.kt
+++ app/src/main/java/com/sewon/healthmonitor/MainActivity.kt
@@ -18,27 +18,27 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        val navView: BottomNavigationView = binding.navView
-
-        val navController = findNavController(nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                navigation_home,
-                navigation_dashboard,
-                navigation_notifications,
-                navigation_agreement
-
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+//        super.onCreate(savedInstanceState)
+//
+//        binding = ActivityMainBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//
+//        val navView: BottomNavigationView = binding.navView
+//
+//        val navController = findNavController(nav_host_fragment_activity_main)
+//        // Passing each menu ID as a set of Ids because each
+//        // menu should be considered as top level destinations.
+//        val appBarConfiguration = AppBarConfiguration(
+//            setOf(
+//                navigation_home,
+//                navigation_dashboard,
+//                navigation_notifications,
+//                navigation_agreement
+//
+//            )
+//        )
+//        setupActionBarWithNavController(navController, appBarConfiguration)
+//        navView.setupWithNavController(navController)
 
 
 
