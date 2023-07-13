9e85c068c521f57c04b1f811c6bbad547eff9495
Thai Phan <thaiphan2712@gmail.com>
2023-07-13 18:10:12 +0900

    add Hilt

---
+++ app/src/main/java/com/sewon/healthmonitor/screen/activity/SleepActivityViewModel.kt
@@ -0,0 +1,11 @@
+package com.sewon.healthmonitor.screen.activity
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class SleepActivityViewModel: ViewModel() {
+
+
+
+}
