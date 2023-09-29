d24d09fd2f1b5e23d10ba1947c6ad1ca1c01b08f
thai-phan <thaiphan2712@gmail.com>
2023-09-29 12:02:34 +0900

    add version catalog

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,13 +6,24 @@ pluginManagement {
     }
 }
 dependencyResolutionManagement {
+    versionCatalogs {
+        create("kotlinx") {
+            from(files("gradle/kotlinx.versions.toml"))
+        }
+        create("androidx") {
+            from(files("gradle/androidx.versions.toml"))
+        }
+        create("compose") {
+            from(files("gradle/compose.versions.toml"))
+        }
+    }
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
-        maven { url 'https://jitpack.io' }
+        maven(url = "https://www.jitpack.io")
 
     }
 }
 rootProject.name = "health-monitor-app"
-include ':app-topper'
+include(":app-topper")
